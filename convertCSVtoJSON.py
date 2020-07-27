b9f9e80734efbe5ca8c0cd3c79ea798704eb33f9
werowe <werowe@walkerrowe.com>
2020-07-27 14:28:23 +0300

    Update convertCSVtoJSON.py

--- convertCSVtoJSON.py
+++ convertCSVtoJSON.py
@@ -1,16 +1,29 @@
+
+
 import simplejson as json
 import os
 
-fileStr='/Users/walkerrowe/Documents/imdb/100.basics.tsv'
+fileStr='/Users/walkerrowe/Documents/customers.csv'
 f = open(fileStr, 'r')
-o = open('/Users/walkerrowe/Documents/imdb/100.basics.json', 'w')
+o = open('/Users/walkerrowe/Documents/customers.json', 'w')
 
 
 def formatJSON(l, cols):
     json = {}
 
     for i in range(len(l)):
-        json[cols[i]] = {"S": l[i]}
+        json[cols[i]] = l[i]
+
+    return json
+
+
+cols = []
+
+def formatJSON(l, cols):
+    json = {}
+
+    for i in range(len(l)):
+        json[cols[i]] = l[i]
 
     return json
 
@@ -21,25 +34,23 @@ cols = []
 
 i = 0
 filePos=0
-f.seek(0, os.SEEK_END)
 f.seek(0)
+o.write("[")
 
 for l in f:
-    ln = l.strip("\\N").strip("\n")
     filePos = filePos + len(l)
     f.seek(filePos)
-    line = ln.split('\t')
+    line = l.split(',')
     if i == 0:
         i = 1
         cols = line
     else:
         if filePos < os.stat(fileStr).st_size:
-            str = json.dumps(formatJSON(line, cols)) + '},\n'
-            o.write(json.dumps(formatJSON(line, cols)) + '},\n')
+           o.write(json.dumps(formatJSON(line, cols)) + ',\n')
         else:
-            str = json.dumps(formatJSON(line, cols)) + '\n'
-            o.write(str)
+           o.write(json.dumps(formatJSON(line, cols)) + '\n')
 
 
+o.write("]")
 
 o.close()
