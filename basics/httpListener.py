af5a0db4ee8b9c6b4e18f7c5b9f15339c6d0c925
werowe <werowe@walkerrowe.com>
2020-09-09 15:07:11 +0300

    Create httpListener.py

---
+++ basics/httpListener.py
@@ -0,0 +1,27 @@
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        #self.wfile.write('callback received')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(('(non loopback address of your server)', 4443), SimpleHTTPRequestHandler)
+
+httpd.serve_forever()
