5eb184550b4f6ecc2046953bbf54ec512eaff166
werowe <werowe@walkerrowe.com>
2026-07-02 05:55:01 +0300

    Create led_off.ino

---
+++ arduino/led_off.ino
@@ -0,0 +1,13 @@
+#define LED 13
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13, HIGH);
+}
